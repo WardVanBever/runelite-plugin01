910b51792be97a1ac759263aecadd50e4686dcf4
WardVanBever <Ward.van.bever@student.howest.be>
2021-02-11 17:30:51 +0100

    Initial setup

--- src/plugin01/java/com/plugin01/Plugin01Plugin.java
+++ src/plugin01/java/com/plugin01/Plugin01Plugin.java
@@ -1,4 +1,4 @@
-package com.example;
+package com.plugin01;
 
 import com.google.inject.Provides;
 import javax.inject.Inject;
@@ -14,15 +14,15 @@ import net.runelite.client.plugins.PluginDescriptor;
 
 @Slf4j
 @PluginDescriptor(
-	name = "Example"
+	name = "Plugin 01"
 )
-public class ExamplePlugin extends Plugin
+public class Plugin01Plugin extends Plugin
 {
 	@Inject
 	private Client client;
 
 	@Inject
-	private ExampleConfig config;
+	private Plugin01Config config;
 
 	@Override
 	protected void startUp() throws Exception
@@ -46,8 +46,8 @@ public class ExamplePlugin extends Plugin
 	}
 
 	@Provides
-	ExampleConfig provideConfig(ConfigManager configManager)
+	Plugin01Config provideConfig(ConfigManager configManager)
 	{
-		return configManager.getConfig(ExampleConfig.class);
+		return configManager.getConfig(Plugin01Config.class);
 	}
 }
--- src/test/java/com/plugin01/ExamplePluginTest.java
+++ src/test/java/com/plugin01/ExamplePluginTest.java
@@ -1,4 +1,4 @@
-package com.example;
+package com.plugin01;
 
 import net.runelite.client.RuneLite;
 import net.runelite.client.externalplugins.ExternalPluginManager;
@@ -7,7 +7,7 @@ public class ExamplePluginTest
 {
 	public static void main(String[] args) throws Exception
 	{
-		ExternalPluginManager.loadBuiltin(ExamplePlugin.class);
+		ExternalPluginManager.loadBuiltin(Plugin01Plugin.class);
 		RuneLite.main(args);
 	}
 }
